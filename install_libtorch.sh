4d3f7d9e212c4ba861340e75b221304888dd9184
root <root@DESKTOP-THFV0FD.localdomain>
2022-11-18 23:30:46 +0100

    Bash file to install LibTorch for C++

---
+++ install_libtorch.sh
@@ -0,0 +1,9 @@
+NC="\033[0m"
+GREEN="\033[0;32m"
+
+echo -e " ${BLUE}------------- Install LibTorch for C++  -------------------${NC}"
+
+
+wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcpu.zip
+unzip libtorch-cxx11-abi-shared-with-deps-1.13.0+cpu.zip
+  
