2f3ae873906465a3f29c06bdd87413c66bc94fb4
root <root@DESKTOP-THFV0FD.localdomain>
2022-11-18 23:46:47 +0100

    Install some Python Libraries

--- install_libtorch.sh
+++ install_libtorch.sh
@@ -6,4 +6,18 @@ echo -e " ${BLUE}------------- Install LibTorch for C++  -------------------${NC
 
 wget https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-1.13.0%2Bcpu.zip
 unzip libtorch-cxx11-abi-shared-with-deps-1.13.0+cpu.zip
-  
+
+
+
+echo -e " ${BLUE}------------  install some Python Libraries   ------------${NC}"
+
+sudo apt install -y python3
+apt install -y python3-pip
+
+sudo apt-get install -y python3-matplotlib
+sudo apt-get install -y python3-opencv
+pip install -y tqdm
+
+pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cpu
+
+
